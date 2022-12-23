ba346659d53e6914301942235a999b299e14b26b
Kirill <kirill.rymar@gmail.com>
2022-12-24 00:35:35 +0200

    first commit

---
+++ Homework2/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+Console.WriteLine("введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+ArrayRandomNumbers(array);
+PrintArray(array);
+ReturnArray(array);
+
+void ArrayRandomNumbers(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = new Random().Next(1,100);
+}
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write($"{array[i, j]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+void ReturnArray(int[,] array)
+{
+Console.WriteLine("введите строку");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите столбец");
+int b = int.Parse(Console.ReadLine());
+ if ( a>=m || b>=n)
+ Console.WriteLine("Такого числа в массиве нет");
+ else
+ {
+ object result = array.GetValue(a,b);
+ Console.WriteLine(result);
+ }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+int[] summ = new int[m];
+ArrayRandomNumbers(array);
+PrintArray(array);
+ArithmeticMeanOfElements(array);
+
+void ArrayRandomNumbers(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = new Random().Next(1,10);
+}
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write($"{array[i, j]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void ArithmeticMeanOfElements(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        summ[i] += array[j, i];
+    }
+}
+            foreach (double result in summ)
+            {
+           Console.Write($"{Math.Round(result/m,1)}; ");
+            }
+    }
